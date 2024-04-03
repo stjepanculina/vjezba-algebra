9edd0e4060cefc0ef6da8962f0460c2bd8721f8e
stjepanculina <stjepan.culina93@gmail.com>
2024-04-03 23:04:29 +0200

    funkcija dodana te pokrenuta

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const gitSomething = {
-    niz: neki[{num:1, num:2},[1,2,3,4,5],'string',false,true],
+    niz: [{num:1, num:2},[1,2,3,4,5],'string',false,true],
+    add: function (a, b) {
+        return a + b;
+    }
 }
+
+const product = gitSomething.add(2, 2);
+console.log(product);
